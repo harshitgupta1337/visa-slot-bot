30b81008a9a7b967e33c2d8aad16d11529f71bee
Harshit Gupta <guptaharshit@microsoft.com>
2023-12-30 18:11:50 -0800

    Fwding works

--- tele_visa_script.py
+++ tele_visa_script.py
@@ -2,8 +2,8 @@ from telethon import TelegramClient, events, sync
 import os
 
 #get this by creating a new bot. Message BotFather with /newbot request
-api_id = API_ID_HERE
-api_hash = 'YOUR API HASH'
+api_id = 22560527
+api_hash = 'cf3530dae96b1931d98691e59b385221'
 client = TelegramClient('anon', api_id, api_hash)
 
 #This function creates a notification on MacOS when the python script is running
@@ -13,13 +13,13 @@ def notify(title, text):
               """.format(text, title))
 
 #This function listens for new messages in the dropbox channel. You can change it to the VAC channel if you want
-@client.on(events.NewMessage(chats='H1B_H4_Visa_Dropbox_slots'))    #select channel you want to monitor
+@client.on(events.NewMessage(chats='USB1B2VisaSlots'))    #select channel you want to monitor
 async def my_event_handler(event):
     print(event.raw_text)
     if event.photo:
-        notify("image -", event.raw_text)
+        #notify("image -", event.raw_text) # Not notifying in desktop app
         await client.send_message(entity=entity,message=event.message)
 
 client.start()
-entity=client.get_entity('YOUR CHANNEL HERE')  #channel you want to forward screenshots to
+entity=client.get_entity('hgcheeku')  #channel you want to forward screenshots to
 client.run_until_disconnected()
